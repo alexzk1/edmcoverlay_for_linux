9a46d5758ef5a85b63e39d99f2280600c3e226eb
Oleksiy Zakharov <alexzkhr@gmail.com>
2025-12-29 02:13:10 +0200

    Update build script, so can switch Release/Debug by parameter given.

--- create_binary.sh
+++ create_binary.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+#You can use Release or Debug paramater to the script.
+MODE=${1:-Release}
+
 #First find the folder, where this script resides.
 SOURCE=${BASH_SOURCE[0]}
 while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -13,7 +16,7 @@ DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 #We expect subfolder cpp there.
 cd $DIR/cpp
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_BUILD_TYPE=${MODE}
 cmake --build build  --verbose
 
 FONT_FILE=./build/AppleColorEmoji.ttf
@@ -25,5 +28,4 @@ fi
 
 echo ""
 echo ""
-echo "Everything is prepared. You can start EDMC now."
-
+echo "Everything is prepared in '${MODE}' mode. You can start EDMC now."
