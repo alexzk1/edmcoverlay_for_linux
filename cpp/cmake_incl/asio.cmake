d74255967f6157d46296a96df0103227865eb69b
Oleksiy Zakharov <alexzkhr@gmail.com>
2026-02-02 15:48:58 +0200

    Update asio cmake file.

--- cpp/cmake_incl/asio.cmake
+++ cpp/cmake_incl/asio.cmake
@@ -3,9 +3,15 @@
 
 #https://github.com/cpm-cmake/CPM.cmake/blob/master/examples/asio-standalone/CMakeLists.txt
 
-CPMAddPackage("gh:chriskohlhoff/asio#asio-1-36-0@1.36.0")
+#Version 1-30-2 is most tested and do not use C++20 yet.
+CPMAddPackage("gh:chriskohlhoff/asio#asio-1-30-2@1.30.2")
 
-add_library(asio INTERFACE)
-target_include_directories(asio SYSTEM INTERFACE ${asio_SOURCE_DIR}/asio/include)
-target_compile_definitions(asio INTERFACE ASIO_STANDALONE ASIO_NO_DEPRECATED)
-target_link_libraries(asio INTERFACE Threads::Threads)
+add_library(asio_lib INTERFACE)
+if(asio_ADDED)
+   target_include_directories(asio_lib SYSTEM INTERFACE ${asio_SOURCE_DIR}/asio/include)
+   target_compile_definitions(asio_lib INTERFACE
+       ASIO_STANDALONE
+       ASIO_NO_DEPRECATED
+   )
+   target_link_libraries(asio_lib INTERFACE Threads::Threads)
+endif()
